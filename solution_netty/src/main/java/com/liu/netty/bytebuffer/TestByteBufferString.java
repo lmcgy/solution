afb22ee45a0f5dcf81c546b4991c87c350ff76fa
liumiao <@miao.liu>
2022-07-19 23:25:43 +0800

    TestByteBufferString

--- solution_netty/src/main/java/com/liu/netty/bytebuffer/TestByteBufferString.java
+++ solution_netty/src/main/java/com/liu/netty/bytebuffer/TestByteBufferString.java
@@ -11,12 +11,18 @@ import java.nio.charset.StandardCharsets;
 public class TestByteBufferString {
 
     public static void main(String[] args) {
-        ByteBuffer allocate = ByteBuffer.allocate(16);
+
         // 1. 字符串转bytebuffer
+        ByteBuffer allocate = ByteBuffer.allocate(16);
         allocate.put("hello".getBytes());
+        allocate.flip(); // 才能读取到
+        String s1 = StandardCharsets.UTF_8.decode(allocate).toString();
+        System.out.println(s1);
 
         // 2. charset
         ByteBuffer hello = StandardCharsets.UTF_8.encode("hello");
+        String s = StandardCharsets.UTF_8.decode(hello).toString();
+        System.out.println(s);
 
         // 3.wrap
         ByteBuffer wrap = ByteBuffer.wrap("hello".getBytes());
